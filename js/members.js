4b1085668e8f2fbf86b53bcfb5539175096919ea
Brian <brianakbar657@gmail.com>
2025-10-22 18:09:02 +0700

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,1 +1,23 @@
-
+// /js/members.js
+const membersData = [
+  {
+    name: "Brian Juli Akbar",
+    role: "Koordinator & Dokumentator",
+    bio: "Suka membuat dokumentasi dan membangun struktur proyek."
+  },
+  {
+    name: "Nadya",
+    role: "Pengembang Python & Machine Learning",
+    bio: "Berfokus pada pemahaman AI dan implementasi awal ML."
+  },
+  {
+    name: "Napis",
+    role: "Front-end Learner",
+    bio: "Belajar HTML dan CSS untuk membangun tampilan website."
+  },
+  {
+    name: "Rizal",
+    role: "UI/UX Designer",
+    bio: "Mendesain tampilan dan pengalaman pengguna Dashkoo."
+  }
+];
